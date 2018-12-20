dc90882105af344505918c5cc0ee131c42313fe2
Бабин Даниил <daniilbi99@gmail.com>
2018-12-20 09:09:20 +0300

    Add files via upload

--- sm05/1/main.c
+++ sm05/1/main.c
@@ -1,19 +1,18 @@
-int mystrcmp(const char *str1, const char *str2)
-{
-    const char *c1 = str1;
-    const char *c2 = str2;
+int mystrcmp(const char *str1, const char *str2) {
+    const unsigned char *c1 = (const unsigned char*)str1;
+    const unsigned char *c2 = (const unsigned char*)str2;
     while(*c1 && *c2) {
-	if(*c1 < *c2) {
-	    return -1;
-	} else if (*c1 > *c2) {
-	    return 1;
-	}
-	++c1;
-	++c2;
+    if(*c1 < *c2) {
+        return -1;
+    } else if (*c1 > *c2) {
+        return 1;
+    }
+    ++c1;
+    ++c2;
     }
     if (*c1)
-        return -1;
-    if (*c2)
         return 1;
+    if (*c2)
+        return -1;
     return 0;
 }
---
+++ sm05/2/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void normalize_path(char *buf) {
+    char *result = buf;
+    bool Pre_slash = false;
+    while (*buf != '\0') {
+        if (!Pre_slash && *buf == '/') {
+            *result = '/';
+            Pre_slash = true;
+            ++*result;
+        } else if (*buf != '/') {
+            *result = *buf;
+            Pre_slash = false;
+            ++*result;
+        }
+        ++*buf;
+    }
+    *result = '\0';
+}
