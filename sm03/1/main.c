dc7dd1fa64d1b1ca30a078eb93578e04e8008a4f
THNTS <daniilbi99@gmail.com>
2018-12-20 09:06:36 +0300

    35

--- sm03/1/main.c
+++ sm03/1/main.c
@@ -3,7 +3,7 @@
 
 uint32_t satsum(uint32_t v1, uint32_t v2) {
     int32_t result;
-    if (_builtin_add_overflow(v1, v2, &result)) {
+    if (__builtin_add_overflow(v1, v2, &result)) {
         result = 0u - 1u;
         return result;
     } else {
